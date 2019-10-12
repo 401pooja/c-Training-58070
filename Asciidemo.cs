b91264bf933d1b92a275550b542a02a0f4c8cdfe
401pooja <51985080+401pooja@users.noreply.github.com>
2019-10-12 14:36:16 +0530

    Add files via upload

---
+++ Asciidemo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chapter22
+{
+    class Asciidemo
+    {
+        static void Main()
+        {
+            char ch = 'A';
+            
+            Console.WriteLine("{0}",(int)ch);
+            int i = 65;
+            Console.WriteLine("{0}", (char)i);
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
